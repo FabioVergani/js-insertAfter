792c7d95717bb3ffebe014c56ab8b6be7d3002f2
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-09-11 23:31:51 +0200

    Update insertAfter.js

--- insertAfter.js
+++ insertAfter.js
@@ -1,5 +1,7 @@
 function insertAfter(x,a){
- let n=a;
- const e=x,p=n.parentElement;
- if(p){return (n=n.nextSibling)?p.insertBefore(e,n):p.appendChild(e);};
+	const n=a,p=n.parentElement;
+	if(p){
+	 const e=x,s=n.nextSibling;
+	 return s?p.insertBefore(e,s):p.appendChild(e);
+	};
 }
